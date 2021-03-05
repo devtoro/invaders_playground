6f0e4aa38bf41c38da0bd1094228ac85e04af4e4
Manolis Tsilikidis <manolistsilikidis@gmail.com>
2021-03-05 10:49:28 +0200

    Add specs for found/hidden invaders

--- spec/space_invader_sepc.rb
+++ spec/space_invader_sepc.rb
@@ -27,4 +27,22 @@ RSpec.describe SpaceRadar::SpaceInvader, 'Test initialisation and mainfunctional
       expect(SpaceRadar::SpaceInvader.new(file_path).is_a?(SpaceRadar::SpaceInvader)).to be_truthy
     end
   end
+
+  context 'Interaction with invader. Find/Hide' do
+    let(:invader) { SpaceRadar::SpaceInvader.new("#{File.dirname(__FILE__)}/sample_files/si1.txt") }
+
+    it 'Has found attr false, one initialized' do
+      expect(invader.found).to be_falsy
+    end
+
+    it 'Has found attr true, once found!' do
+      invader.found!
+      expect(invader.found).to be_truthy
+    end
+
+    it 'Has found attr false, one hidden' do
+      invader.hide!
+      expect(invader.found).to be_falsy
+    end
+  end
 end
