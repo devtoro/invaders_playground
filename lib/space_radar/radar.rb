ae1dd0abc73dfabf8fab12c70fdc48ec4233f0ca
Manolis Tsilikidis <manolistsilikidis@gmail.com>
2021-03-05 15:08:02 +0200

    Fix initializer and update encapsulation

--- lib/space_radar/radar.rb
+++ lib/space_radar/radar.rb
@@ -5,11 +5,12 @@ module SpaceRadar
   # noise_threshold defines the tollerance for inncorrect matches for evey scan itteration
   # found invaders is a counter
   class Radar < Board
-    attr_reader :noise_threshold, :found_invaders
+    attr_reader :found_invaders
+    attr_writer :noise_threshold
 
     # Default noise_threshold is set to 1
     def initialize(input, noise_threshold: 1)
-      super
+      super(input)
       @noise_threshold = noise_threshold
       @found_invaders = 0
     end
